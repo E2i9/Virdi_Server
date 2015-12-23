e91ed213e1b724f9ed6258fb430a777b201cecee
cezarsantanna <cezar.santanna@gmail.com>
2015-12-23 13:44:47 -0200

    Correção do script para ajustes das vagas e tags

--- set_vagas_tags.py
+++ set_vagas_tags.py
@@ -19,6 +19,12 @@ with psycopg2.connect(database="alpha", user="cezar") as conn_pg:
                         _vaga_carro_id = conn_pgs2.fetchone()
 			conn_pgs2.execute("select vaga_moto_id from occ_apto where id = (%s);", (_apto_id, ))
                         _vaga_moto_id = conn_pgs2.fetchone()
-			conn_pgs2.execute("INSERT INTO occ_tag_carro_rel (vaga_carro_ids, tag_ids) VALUES (%s, %s);", (_vaga_carro_id, _tag_id))
-                        conn_pgs2.execute("INSERT INTO occ_tag_moto_rel (vaga_moto_ids, tag_ids) VALUES (%s, %s);", (_vaga_moto_id, _tag_id))
+			conn_pgs2.execute("select tipo from occ_tag where id = (%s);", (_tag_id, ))
+			_tag_tipo = reduce(add, conn_pgs2.fetchone())
+			if _tag_tipo == 'carro':
+				conn_pgs2.execute("INSERT INTO occ_tag_carro_rel (vaga_carro_ids, tag_ids) VALUES (%s, %s);", (_vaga_carro_id, _tag_id))
+				conn_pgs2.execute("delete from occ_tag_moto_rel where tag_ids = (%s);", (_tag_id, ))
+			if _tag_tipo == 'moto':
+	                        conn_pgs2.execute("INSERT INTO occ_tag_moto_rel (vaga_moto_ids, tag_ids) VALUES (%s, %s);", (_vaga_moto_id, _tag_id))
+				conn_pgs2.execute("delete from occ_tag_carro_rel where tag_ids = (%s);", (_tag_id, ))
 
